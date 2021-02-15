f68abc0cd0712bcd6a46290b2ad41b90224262f8
Pablo Manuelli <pablo.manuelli@etermax.com>
2021-02-15 02:19:13 -0300

    Bigger font on text editors

--- Fallout Notes/Presentation/Location Edition/Table Cells/ArmorWorkbenchTableViewCell.swift
+++ Fallout Notes/Presentation/Location Edition/Table Cells/ArmorWorkbenchTableViewCell.swift
@@ -24,15 +24,11 @@ class ArmorWorkbenchTableViewCell: UITableViewCell, AutoRegistrableTableViewCell
     
     private func setupSwitchViewOffState(_ switchView: UISwitch) {
         
-        // TODO: Why can't use the exact color (28, 28, 28)? It doesn't show as expected
-        let color = UIColor(white: 0.05, alpha: 1)
-        
+        let color = UIColor.darkGray
         switchView.backgroundColor = color
         switchView.tintColor = color
         switchView.clipsToBounds = true
         switchView.cornerRadius = switchView.bounds.height / 2.0
-        
-        selectionStyle = .none
     }
     
     @objc private func switchValueChanged() {
