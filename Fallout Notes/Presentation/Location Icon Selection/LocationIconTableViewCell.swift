4493e0a6ba4f1011baddfacf822b6d9a20f154fc
Pablo Manuelli <pablo.manuelli@etermax.com>
2021-02-15 13:57:24 -0300

    Changing location selection design

--- Fallout Notes/Presentation/Location Icon Selection/LocationIconTableViewCell.swift
+++ Fallout Notes/Presentation/Location Icon Selection/LocationIconTableViewCell.swift
@@ -5,7 +5,6 @@ class LocationIconTableViewCell: UITableViewCell {
     static let reuseIdentifier: String = "LocationIconTableViewCell"
     static var nib: UINib { UINib(nibName: reuseIdentifier, bundle: Bundle(for: self)) }
 
-    @IBOutlet private var iconBackgroundImageView: UIImageView!
     @IBOutlet private var iconImageView: UIImageView!
     @IBOutlet private var iconNameLabel: UILabel!
     
@@ -21,12 +20,7 @@ class LocationIconTableViewCell: UITableViewCell {
         self.viewModel = viewModel
         
         iconImageView.image = Icons.icon(for: viewModel.type)
-        iconNameLabel.text = viewModel.name
-        
-        GreenBlurEffect.apply(to: iconImageView)
-        
-        let backgroundIndex = (1...2).randomElement()!
-        iconBackgroundImageView.image = UIImage(named: "icon_selection_background_\(backgroundIndex)")
+        iconNameLabel.text = viewModel.name        
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
